4f605f4c82b2526edea17b5dc8179b1b4c5a3bc2
GeissonChagas <geisson.excel@gmail.com>
2023-03-12 23:01:53 -0300

    feat: componentes de barra lateral e formulario

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,39 @@
+<template>
+    <header>
+        <h1 class="logo">ToDo VueTS</h1>
+    </header>
+</template>
+
+<script lang="ts"> // linguaguem TypeScript
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'BarraLateral'
+})
+</script>
+
+<style scoped> /* Estilo do componente (scoped) */
+    header{
+        background: #0d3b66;
+        width: 100%;
+        height: 100vh;
+    }
+
+    .logo{
+        width: 100%;
+        height: 200px;
+        color: #fff;
+        padding: 1rem;
+        font-weight: 700;
+    }
+
+    @media only screen and (max-width: 768px) {
+        header{
+            padding: 2.5rem;
+            height: auto;
+        }
+        
+    }
+</style>
+
