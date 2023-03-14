a840531e7e3fa3f9f37c2a418c26a3d8ea2bb291
GeissonChagas <geisson.excel@gmail.com>
2023-03-13 22:47:49 -0300

    feat: start/stop do cronometro

--- src/components/FormularioComponent.vue
+++ src/components/FormularioComponent.vue
@@ -17,15 +17,15 @@
           class="is-flex is-align-items-center is-justify-content-space-between"
         >
           <section>
-            <strong>00:00:00</strong>
+            <strong>{{ tempoDecorrido }}</strong>
           </section>
-          <button class="button">
+          <button class="button" @click='iniciar'>
             <span class="icon">
               <i class="fas fa-play"></i>
             </span>
             <span>Play</span>
           </button>
-          <button class="button">
+          <button class="button" @click="finalizar">
             <span class="icon">
               <i class="fas fa-stop"></i>
             </span>
@@ -38,10 +38,33 @@
 </template>
 
 <script lang="ts">
+
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "FormularioComponent",
+  data () {
+    return {
+        tempoEmSegundos: 0,
+        cronometro: 0
+      }
+    },
+  computed: {
+    tempoDecorrido() : string {
+      return new Date(this.tempoEmSegundos *  1000).toISOString().substr(11,8)
+    }
+  },
+  methods: {
+    iniciar() {
+      this.cronometro  = setInterval( () => {
+         this.tempoEmSegundos += 1
+        }, 1000)
+        console.log('iniciando')
+    },
+    finalizar (){
+      clearInterval(this.cronometro)
+    }
+  },
 });
 </script>
 
